30e7e86af57cde783aef615d2d6680b192c459c0
Pritesh_PC <priteshrane01@gmail.com>
2020-07-10 19:55:49 +0530

    Firebase added to project, Added required resources to project (image, string)

--- app/src/main/java/com/ransoft/androidnotification/ui/main/MainActivity.kt
+++ app/src/main/java/com/ransoft/androidnotification/ui/main/MainActivity.kt
@@ -2,15 +2,36 @@ package com.ransoft.androidnotification.ui.main
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import androidx.navigation.Navigation
 import androidx.navigation.ui.NavigationUI
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.firebase.iid.FirebaseInstanceId
 import com.ransoft.androidnotification.R
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    private val TAG = "Notification"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        FirebaseInstanceId.getInstance().instanceId
+            .addOnCompleteListener(OnCompleteListener { task ->
+                if (!task.isSuccessful) {
+                    Log.w(TAG, "getInstanceId failed", task.exception)
+                    return@OnCompleteListener
+                }
+
+                // Get new Instance ID token
+                val token = task.result?.token
+
+                // Log and toast
+                val msg = getString(R.string.msg_token_fmt, token)
+                Log.d(TAG, msg)
+                Toast.makeText(baseContext, msg, Toast.LENGTH_SHORT).show()
+            })
 
         setSupportActionBar(toolbar)
 

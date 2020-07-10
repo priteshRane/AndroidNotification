49f172d8aa11a1320d979dd85bbe4070a324a35e
Pritesh_PC <priteshrane01@gmail.com>
2020-07-10 18:18:11 +0530

    Required libraries added to build.gradle, Project structure created

---
+++ app/src/main/java/com/ransoft/androidnotification/ui/home/HomeViewModel.kt
@@ -0,0 +1,7 @@
+package com.ransoft.androidnotification.ui.home
+
+import androidx.lifecycle.ViewModel
+
+class HomeViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
---
+++ app/src/main/java/com/ransoft/androidnotification/ui/main/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.ransoft.androidnotification.ui.main
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.navigation.Navigation
+import androidx.navigation.ui.NavigationUI
+import com.ransoft.androidnotification.R
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        setSupportActionBar(toolbar)
+
+        val navController = Navigation.findNavController(this, R.id.fragment)
+        NavigationUI.setupWithNavController(nav_view, navController)
+        NavigationUI.setupActionBarWithNavController(this, navController, drawer_layout)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return NavigationUI.navigateUp(
+            Navigation.findNavController(this, R.id.fragment),
+            drawer_layout
+        )
+    }
+}
